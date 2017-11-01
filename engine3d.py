206fad1bb9e2aa345e304b3f8a001afe0a5c55a2
Daniel Czerniawski <czerniawski.daniel@gmail.com>
2017-11-01 20:19:27 +0100

    Added comments

--- engine3d.py
+++ engine3d.py
@@ -1,10 +1,19 @@
 import numpy as np
-
+#
+#   Class is defining camera object
+#
 class camera:
-    def __init__(self):
-        self.position = np.array([0., 0., 0.])
-        self.target = np.array([0., 0., 0.])
+    def __init__(self, position, target):
 
+        # Camera position in space
+        self.position = np.array(position)
+
+        # Where is camera looking?
+        self.target = np.array(target)
+
+#
+#   Class is defining shape (mesh). Takes name and vertices count as input
+#
 class mesh:
     def __init__(self, name, verts_count):
         self.name = name
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Render:
         self.background = pygame.image.load("back.jpg").convert()
         self.surface = pygame.Surface((640,480))
         self.surface.fill((255,255,255))
+
     def run(self):
         clock = pygame.time.Clock()
         while True:
@@ -18,24 +19,24 @@ class Render:
                 if event.type == pygame.QUIT:
                     sys.exit(0)
 
-            self.surface.blit(self.background, (0,0))
+            #self.surface.blit(self.background, (0,0))
             pygame.draw.line(self.surface, (0,255,0), [0, 0], [400,30], 1)
             self.screen.blit(self.surface,(0,0))
             pygame.display.update()
 
 
 if __name__=="__main__":
-    c = engine3d.camera()
+    c = engine3d.camera([0.0, 0.0, 10.0], [0.0, 0.0, 0.0])
     cube = engine3d.mesh("Kostka", 8)
-    cube.vertices[0] = np.array([-1., 1., 1.])
-    cube.vertices[1] = np.array([1., 1., 1.])
-    cube.vertices[2] = np.array([-1., -1., 1.])
-    cube.vertices[3] = np.array([-1., -1., -1.])
-    cube.vertices[4] = np.array([-1., 1., -1.])
-    cube.vertices[5] = np.array([1., 1., -1.])
-    cube.vertices[6] = np.array([1., -1., 1.])
-    cube.vertices[7] = np.array([1., -1., -1.])
-    print(cube.vertices[0])
+    cube.vertices[0] = np.array([-1.0, 1.0, 1.0])
+    cube.vertices[1] = np.array([1.0, 1.0, 1.0])
+    cube.vertices[2] = np.array([-1.0, -1.0, 1.0])
+    cube.vertices[3] = np.array([-1.0, -1.0, -1.0])
+    cube.vertices[4] = np.array([-1.0, 1.0, -1.0])
+    cube.vertices[5] = np.array([1.0, 1.0, -1.0])
+    cube.vertices[6] = np.array([1.0, -1.0, 1.0])
+    cube.vertices[7] = np.array([1.0, -1.0, -1.0])
+    print(cube.vertices[:8])
 
     r = Render()
     r.run()
