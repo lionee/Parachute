724ade9917a9b6280e7ecacf2cb8eefb7635cfad
lionee <czerniawski.daniel@gmail.com>
2017-11-02 12:35:30 +0100

    Code cleanup

--- engine3d.py
+++ engine3d.py
@@ -1,6 +1,24 @@
 import numpy as np
+#
+# Point
+#
+class Point:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
 
+#
+# Vertex4
+#
+class Vector4:
+    def __init__(self, x, y, z, w):
+        self.vertex4 = np.array([x, y, z, w])
 
+    def print_vertex4(self):
+        print(self.vertex4)
+
+    def get_nparray(self):
+        return self.vertex4
 #
 #   Class is defining camera object
 #
@@ -20,6 +38,56 @@ class Camera:
 class Mesh:
     def __init__(self, name, verts_count):
         self.name = name
-        self.vertices = [np.array([0., 0., 0.])] * verts_count
+        self.vertices = [np.array(Vector4(0., 0., 0., 0.))] * verts_count
         self.position = np.array([0., 0., 0.])
         self.rotation = np.array([0., 0., 0.])
+        self.mvp = Vector4(0. ,0. ,0. ,0.)
+
+    def Scale(self, x, y, z):
+        s_matrix = self.ScaleMatrix = np.array([[x, 0., 0., 0.],
+                      [0., y, 0., 0.],
+                      [0., 0., z, 0.],
+                      [0., 0., 0., 1.]])
+
+        return s_matrix;
+
+    def Translate(self, x, y, z):
+        t_matrix = self.TranslateMatrix = np.array([[1, 0., 0., x],
+                      [0., 1, 0., y],
+                      [0., 0., 1, z],
+                      [0., 0., 0., 1.]])
+
+        return t_matrix;
+
+    def Transform(self):
+
+        for vert in self.vertices:
+            mvp = self.ScaleMatrix.dot(self.TranslateMatrix.dot(vert.get_nparray()))
+            print(mvp)
+
+    def GetProjectionMatrix(self):
+        return self.mvp;
+
+
+    def Project(self, coord, TransMat):
+        p = Point()
+
+        print(coord)
+        print("DDDD")
+        print(TransMat.get_nparray())
+
+    def Print(self):
+        for vert in self.vertices:
+            vert.print_vertex4()
+
+
+#
+#   Class doing magic
+#
+"""
+class Engine3d:
+    def Render(self, camera, meshes):
+
+    def Project(self, coord, transMatrix):
+        point = TransformCoordinate(coord, transMatrix)
+"""
--- main.py
+++ main.py
@@ -5,12 +5,33 @@ import sys
 import engine3d
 
 
-class Render:
+class Game:
     def __init__(self):
         self.screen = pygame.display.set_mode((640, 480))
         self.background = pygame.image.load("back.jpg").convert()
         self.surface = pygame.Surface((640, 480))
         self.surface.fill((255, 255, 255))
+
+        c = engine3d.Camera([0.0, 0.0, 10.0], [0.0, 0.0, 0.0])
+        cube = engine3d.Mesh("Kostka", 8)
+        cube.vertices[0] = engine3d.Vector4(-0.0, 0.0, 0.0, 0.0)
+        cube.vertices[1] = engine3d.Vector4(1.0, 1.0, 1.0, 1.0)
+        cube.vertices[2] = engine3d.Vector4(-1.0, -1.0, 1.0, 0.0)
+        cube.vertices[3] = engine3d.Vector4(-1.0, -1.0, -1.0, 0.0)
+        cube.vertices[4] = engine3d.Vector4(-1.0, 1.0, -1.0, 0.0)
+        cube.vertices[5] = engine3d.Vector4(1.0, 1.0, -1.0, 0.0)
+        cube.vertices[6] = engine3d.Vector4(1.0, -1.0, 1.0, 0.0)
+        cube.vertices[7] = engine3d.Vector4(1.0, -1.0, -1.0, 0.0)
+
+        # translate into world space
+        cube.Translate(2.0, 2.0, 2.0)
+
+        # scale mesh
+        cube.Scale(4.0, 3.0, 5.0)
+
+        # There should be projection
+        cube.Transform()
+        cube.Project(cube.vertices[1].get_nparray(), cube.GetProjectionMatrix())
 
     def run(self):
         clock = pygame.time.Clock()
@@ -27,17 +48,7 @@ class Render:
 
 
 if __name__ == "__main__":
-    c = engine3d.Camera([0.0, 0.0, 10.0], [0.0, 0.0, 0.0])
-    cube = engine3d.Mesh("Kostka", 8)
-    cube.vertices[0] = np.array([-1.0, 1.0, 1.0])
-    cube.vertices[1] = np.array([1.0, 1.0, 1.0])
-    cube.vertices[2] = np.array([-1.0, -1.0, 1.0])
-    cube.vertices[3] = np.array([-1.0, -1.0, -1.0])
-    cube.vertices[4] = np.array([-1.0, 1.0, -1.0])
-    cube.vertices[5] = np.array([1.0, 1.0, -1.0])
-    cube.vertices[6] = np.array([1.0, -1.0, 1.0])
-    cube.vertices[7] = np.array([1.0, -1.0, -1.0])
-    print(cube.vertices[:8])
 
-    r = Render()
-    r.run()
+
+    g = Game()
+    g.run()
