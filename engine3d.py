909250499d6723181e3f463fcbac05cf1ed68114
lionee <czerniawski.daniel@gmail.com>
2017-11-06 14:38:19 +0100

    First running cube

--- engine3d.py
+++ engine3d.py
@@ -1,24 +1,30 @@
 import numpy as np
-#
-# Point
-#
-class Point:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
+import pygame
+from pygame import gfxdraw
+import math
+
 
 #
 # Vertex4
 #
 class Vector4:
     def __init__(self, x, y, z, w):
-        self.vertex4 = np.array([x, y, z, w])
+        self.vector = np.array([x, y, z, w])
+
+    def retx(self):
+        return self.vector[0]
+
+    def rety(self):
+        return self.vector[1]
+
+    def retz(self):
+        return self.vector[2]
 
     def print_vertex4(self):
-        print(self.vertex4)
+        print(self.vector)
 
     def get_nparray(self):
-        return self.vertex4
+        return self.vector
 #
 #   Class is defining camera object
 #
@@ -38,10 +44,11 @@ class Camera:
 class Mesh:
     def __init__(self, name, verts_count):
         self.name = name
-        self.vertices = [np.array(Vector4(0., 0., 0., 0.))] * verts_count
+        self.vertices = [np.array([0., 0., 0., 0.])] * verts_count
         self.position = np.array([0., 0., 0.])
         self.rotation = np.array([0., 0., 0.])
         self.mvp = Vector4(0. ,0. ,0. ,0.)
+        self.points2d = np.array([0, 0]) * verts_count
 
     def Scale(self, x, y, z):
         s_matrix = self.ScaleMatrix = np.array([[x, 0., 0., 0.],
@@ -49,45 +56,68 @@ class Mesh:
                       [0., 0., z, 0.],
                       [0., 0., 0., 1.]])
 
+        for vert in self.vertices:
+            vert.vector = vert.vector.dot(s_matrix)
+
         return s_matrix;
 
+    def Rotate(self, radiansx, radiansy):
+
+        rsinx = math.sin(radiansx)
+        rcosx = math.cos(radiansx)
+
+        rsiny= math.sin(radiansy)
+        rcosy= math.cos(radiansy)
+
+        # Rotation Matrix around X axis
+        rx_matrix = self.RotationXMatrix = np.array([[rcosx, 0., rsinx, 0.],
+                                                [0., 1, 0., 0.],
+                                                [-rsinx, 0., rcosx, 0.],
+                                                [0., 0., 0., 1.]])
+        # Rotation Matrix around Y axis
+        ry_matrix = self.RotationYMatrix = np.array([[1, 0., 0, 0.],
+                                                [0., rcosy, -rsiny, 0.],
+                                                [0, rsiny, rcosy, 0.],
+                                                [0., 0., 0., 1.]])
+        for vert in self.vertices:
+            vert.vector = vert.vector.dot(rx_matrix.dot(ry_matrix))
+
+        return rx_matrix;
+
+
     def Translate(self, x, y, z):
-        t_matrix = self.TranslateMatrix = np.array([[1, 0., 0., x],
-                      [0., 1, 0., y],
-                      [0., 0., 1, z],
+        t_matrix = self.TranslateMatrix = np.array([[1., 0., 0., x],
+                      [0., 1., 0., y],
+                      [0., 0., 1., z],
                       [0., 0., 0., 1.]])
+
+        for vert in self.vertices:
+            vert.vector = vert.vector.dot(t_matrix)
 
         return t_matrix;
 
-    def Transform(self):
 
+    def Render(self, surface):
+        surface.fill((255, 255, 255))
+        v = Vector4(0. ,0. ,0. ,0.)
         for vert in self.vertices:
-            mvp = self.ScaleMatrix.dot(self.TranslateMatrix.dot(vert.get_nparray()))
-            print(mvp)
+
+            v = vert.vector[2]
+            vert.vector[2] += 15
+
+            x = vert.retx()*(300/vert.retz())
+            y = vert.rety()*(300/vert.retz())
+            print((int(x) + int(surface.get_width()/2), int(y) + int(surface.get_height()/2)))
+
+            # TODO - Clipping!
+
+            #if (x>=0 and y>=0 and x<surface.get_width() and y<surface.get_height()):
+            pygame.draw.circle(surface, (0,0,0), (int(x) + int(surface.get_width()/2), int(y) + int(surface.get_height()/2)), 4)
+            vert.vector[2] = v
+
 
     def GetProjectionMatrix(self):
         return self.mvp;
 
 
-    def Project(self, coord, TransMat):
-        p = Point()
 
-        print(coord)
-        print("DDDD")
-        print(TransMat.get_nparray())
-
-    def Print(self):
-        for vert in self.vertices:
-            vert.print_vertex4()
-
-
-#
-#   Class doing magic
-#
-"""
-class Engine3d:
-    def Render(self, camera, meshes):
-
-    def Project(self, coord, transMatrix):
-        point = TransformCoordinate(coord, transMatrix)
-"""
--- main.py
+++ main.py
@@ -7,31 +7,18 @@ import engine3d
 
 class Game:
     def __init__(self):
-        self.screen = pygame.display.set_mode((640, 480))
+        self.w = 640
+        self.h = 480
+        self.screen = pygame.display.set_mode((self.w, self.h))
         self.background = pygame.image.load("back.jpg").convert()
         self.surface = pygame.Surface((640, 480))
         self.surface.fill((255, 255, 255))
 
-        c = engine3d.Camera([0.0, 0.0, 10.0], [0.0, 0.0, 0.0])
-        cube = engine3d.Mesh("Kostka", 8)
-        cube.vertices[0] = engine3d.Vector4(-0.0, 0.0, 0.0, 0.0)
-        cube.vertices[1] = engine3d.Vector4(1.0, 1.0, 1.0, 1.0)
-        cube.vertices[2] = engine3d.Vector4(-1.0, -1.0, 1.0, 0.0)
-        cube.vertices[3] = engine3d.Vector4(-1.0, -1.0, -1.0, 0.0)
-        cube.vertices[4] = engine3d.Vector4(-1.0, 1.0, -1.0, 0.0)
-        cube.vertices[5] = engine3d.Vector4(1.0, 1.0, -1.0, 0.0)
-        cube.vertices[6] = engine3d.Vector4(1.0, -1.0, 1.0, 0.0)
-        cube.vertices[7] = engine3d.Vector4(1.0, -1.0, -1.0, 0.0)
 
-        # translate into world space
-        cube.Translate(2.0, 2.0, 2.0)
 
-        # scale mesh
-        cube.Scale(4.0, 3.0, 5.0)
 
-        # There should be projection
-        cube.Transform()
-        cube.Project(cube.vertices[1].get_nparray(), cube.GetProjectionMatrix())
+
+
 
     def run(self):
         clock = pygame.time.Clock()
@@ -41,14 +28,28 @@ class Game:
                 if event.type == pygame.QUIT:
                     sys.exit(0)
 
-            # self.surface.blit(self.background, (0, 0))
-            pygame.draw.line(self.surface, (0, 255, 0), [0, 0], [400, 30], 1)
+
+            cube.Rotate(0.01, 0.02)
+            cube.Render(self.surface)
             self.screen.blit(self.surface, (0, 0))
             pygame.display.update()
 
 
 if __name__ == "__main__":
 
+    #c = engine3d.Camera([0.0, 0.0, 10.0], [0.0, 0.0, 0.0])
+    cube = engine3d.Mesh("Kostka", 8)
+    cube.vertices[0] = engine3d.Vector4(-1.0, 1.0, 1.0, 0.0)
+    cube.vertices[1] = engine3d.Vector4(1.0, 1.0, 1.0, 1.0)
+    cube.vertices[2] = engine3d.Vector4(-1.0, -1.0, 1.0, 0.0)
+    cube.vertices[3] = engine3d.Vector4(-1.0, -1.0, -1.0, 0.0)
+    cube.vertices[4] = engine3d.Vector4(-1.0, 1.0, -1.0, 0.0)
+    cube.vertices[5] = engine3d.Vector4(1.0, 1.0, -1.0, 0.0)
+    cube.vertices[6] = engine3d.Vector4(1.0, -1.0, 1.0, 0.0)
+    cube.vertices[7] = engine3d.Vector4(1.0, -1.0, -1.0, 0.0)
+
+    # Initial scale mesh
+    cube.Scale(4.5, 4.5, 4.5)
 
     g = Game()
     g.run()
