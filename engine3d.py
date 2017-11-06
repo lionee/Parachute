a13c29aa69163b658e3fc76b093bed45107531a2
Daniel Czerniawski <czerniawski.daniel@gmail.com>
2017-11-06 18:46:22 +0100

    Added edges rendering.

--- engine3d.py
+++ engine3d.py
@@ -47,8 +47,10 @@ class Mesh:
         self.vertices = [np.array([0., 0., 0., 0.])] * verts_count
         self.position = np.array([0., 0., 0.])
         self.rotation = np.array([0., 0., 0.])
-        self.mvp = Vector4(0. ,0. ,0. ,0.)
-        self.points2d = np.array([0, 0]) * verts_count
+        # todo: make below simpler!
+        self.edges = np.array([[0, 1],[1, 2],[2, 3],[3, 0],[4,5],[5,6],[6,7],[7,4],[0,4],[1,5],[2,6],[3,7]])
+
+
         print("Mesh created")
 
     def Scale(self, x, y, z):
@@ -99,7 +101,7 @@ class Mesh:
 
 
     def Render(self, surface):
-        surface.fill((255, 255, 255))
+        surface.fill((255, 255, 0))
         v = Vector4(0. ,0. ,0. ,0.)
         for vert in self.vertices:
 
@@ -113,14 +115,21 @@ class Mesh:
             # TODO - Clipping!
 
             #if (x>=0 and y>=0 and x<surface.get_width() and y<surface.get_height()):
-            pygame.draw.circle(surface, (0,0,0), (int(x) + int(surface.get_width()/2), int(y) + int(surface.get_height()/2)), 4)
+            pygame.draw.circle(surface, (0,0,0), (int(x) + int(surface.get_width()/2), int(y) + int(surface.get_height()/2)), 2)
             #print(int(x))
-            #pygame.gfxdraw.aacircle(surface, x + surface.get_width()/2, y + surface.get_height()/2, 4 (0,0,0))
+            #pygame.gfxdraw.aacircle(surface, int(x) + int(surface.get_width()/2), int(y) + int(surface.get_height()/2), 1, (0,0,0))
             vert.vector[2] = v
 
+        #   Render edges!
+        for edge in self.edges:
+            points =[]
 
-    def GetProjectionMatrix(self):
-        return self.mvp;
+            for vertedge in (self.vertices[edge[0]], self.vertices[edge[1]]):
+                ev = vertedge.vector[2]
+                vertedge.vector[2] += 15
+                ex = vertedge.retx() * (300 / vertedge.retz())
+                ey = vertedge.rety() * (300 / vertedge.retz())
+                points+=[(int(ex) + int(surface.get_width()/2), int(ey) + int(surface.get_height()/2))]
 
-
-
+                vertedge.vector[2]=ev
+            pygame.draw.line(surface, (0,0,0), points[0], points[1], 1)
--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import engine3d
 
 class Game:
     def __init__(self):
-        self.w = 640
-        self.h = 480
+        self.w = 1280
+        self.h = 720
         self.screen = pygame.display.set_mode((self.w, self.h))
         print("Display initialized")
         self.background = pygame.image.load("back.jpg").convert()
-        self.surface = pygame.Surface((640, 480))
+        self.surface = pygame.Surface((self.w, self.h))
         self.surface.fill((255, 255, 255))
 
     def run(self):
@@ -42,17 +42,17 @@ if __name__ == "__main__":
 
     # Since we have no 3d model - we make our cube by hand...
 
-    cube.vertices[0] = engine3d.Vector4(-1.0, 1.0, 1.0, 0.0)
-    cube.vertices[1] = engine3d.Vector4(1.0, 1.0, 1.0, 1.0)
-    cube.vertices[2] = engine3d.Vector4(-1.0, -1.0, 1.0, 0.0)
-    cube.vertices[3] = engine3d.Vector4(-1.0, -1.0, -1.0, 0.0)
-    cube.vertices[4] = engine3d.Vector4(-1.0, 1.0, -1.0, 0.0)
-    cube.vertices[5] = engine3d.Vector4(1.0, 1.0, -1.0, 0.0)
-    cube.vertices[6] = engine3d.Vector4(1.0, -1.0, 1.0, 0.0)
-    cube.vertices[7] = engine3d.Vector4(1.0, -1.0, -1.0, 0.0)
+    cube.vertices[0] = engine3d.Vector4(-1.0, -1.0, -1.0, 0.0)
+    cube.vertices[1] = engine3d.Vector4(1.0, -1.0, -1.0, 0.0)
+    cube.vertices[2] = engine3d.Vector4(1.0, 1.0, -1.0, 0.0)
+    cube.vertices[3] = engine3d.Vector4(-1.0, 1.0, -1.0, 0.0)
+    cube.vertices[4] = engine3d.Vector4(-1.0, -1.0, 1.0, 0.0)
+    cube.vertices[5] = engine3d.Vector4(1.0, -1.0, 1.0, 0.0)
+    cube.vertices[6] = engine3d.Vector4(1.0, 1.0, 1.0, 0.0)
+    cube.vertices[7] = engine3d.Vector4(-1.0, 1.0, 1.0, 0.0)
 
     # Initial scale mesh
-    cube.Scale(4.5, 4.5, 4.5)
+    cube.Scale(4., 4., 4.)
 
     g = Game()
     g.run()
